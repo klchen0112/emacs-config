33d8daf00877d69890e3645a5c9d1e07980e5dc8
klchen <klchen0112@gmail.com>
2025-10-06 09:22:25 +0000

    themes: add nerd-icons, dashboard and modeline

--- lock/flake.nix
+++ lock/flake.nix
@@ -7,16 +7,40 @@
       repo = "benchmark-init-el";
       type = "github";
     };
+    compat = {
+      flake = false;
+      owner = "emacs-compat";
+      repo = "compat";
+      type = "github";
+    };
     dash = {
       flake = false;
       owner = "magnars";
       repo = "dash.el";
       type = "github";
     };
+    dashboard = {
+      flake = false;
+      owner = "emacs-dashboard";
+      repo = "emacs-dashboard";
+      type = "github";
+    };
+    doom-modeline = {
+      flake = false;
+      owner = "seagle0128";
+      repo = "doom-modeline";
+      type = "github";
+    };
     ef-themes = {
       flake = false;
       owner = "protesilaos";
       repo = "ef-themes";
+      type = "github";
+    };
+    "f" = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
       type = "github";
     };
     gcmh = {
@@ -43,28 +67,28 @@
       repo = "modus-themes";
       type = "github";
     };
-    popup = {
+    nerd-icons = {
       flake = false;
-      owner = "auto-complete";
-      repo = "popup-el";
+      owner = "rainstormstudio";
+      repo = "nerd-icons.el";
       type = "github";
     };
-    posframe = {
+    "s" = {
       flake = false;
-      owner = "tumashu";
-      repo = "posframe";
-      type = "github";
-    };
-    rime = {
-      flake = false;
-      owner = "DogLooksGood";
-      repo = "emacs-rime";
+      owner = "magnars";
+      repo = "s.el";
       type = "github";
     };
     setup = {
       flake = false;
       type = "git";
       url = "https://codeberg.org/pkal/setup.el";
+    };
+    shrink-path = {
+      flake = false;
+      owner = "bennya";
+      repo = "shrink-path.el";
+      type = "gitlab";
     };
     sis = {
       flake = false;
